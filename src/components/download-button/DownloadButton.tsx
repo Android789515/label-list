8cdcebd713628713a6996e5dc79f221d781ce537
Android789515 <derflug@mailfence.com>
2024-12-13 13:30:28 -0600

    Download Functionality

--- src/components/download-button/DownloadButton.tsx
+++ src/components/download-button/DownloadButton.tsx
@@ -7,11 +7,26 @@ interface Props {
 }
 
 export const DownloadButton = ({ data }: Props) => {
+  const labels = data.map(label => label.label).join('\n');
+
+  const labelsFile = new File([labels], 'labels.txt', {
+    type: 'text/plain',
+  });
+
+  const url = URL.createObjectURL(labelsFile);
+
   return (
-    <button
+    <a
+      href={url}
+      download={labelsFile.name}
       className={styles.downloadButton}
+      onClick={() => {
+        setTimeout(() => {
+          window.URL.revokeObjectURL(url);
+        }, 1000);
+      }}
     >
       Download Wire Labels
-    </button>
+    </a>
   );
 }
